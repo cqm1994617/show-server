895a1d9331d364503d37d4fb716d2e3ea44dfa3d
cqm <1025760508@qq.com>
2016-04-13 15:15:00 +0800

    koa2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-/**
- * Created by tdzl2003 on 3/7/16.
- */
-
 import app from './app';
 import path from 'path';
 import doMigrate from './utils/doMigrate';
+import {
+  User,
+  sequelize
+} from '../models';
+//import Sequelize from 'sequelize';
+
+//var sequelize = new Sequelize('show_development', 'root', '86163169');
 
 async function main() {
-  const port = process.env['PORT'] || 8901;
+  const port = process.env['PORT'] || 3000;
   const host = process.env['HOST'] || '';
 
   await doMigrate();
@@ -15,7 +18,7 @@ async function main() {
     const server = app.listen(port, host, res);
     server.on('error', rej);
   });
-  console.log('Ready');
+  console.log('Ready!');
 }
 
 main()
